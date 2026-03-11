8cebe3bcaaf650ee58178bbef436fa24aa2cd01c
Paul Reman <65947893+Paparoto@users.noreply.github.com>
2026-03-11 11:45:25 +0100

    Update 2_contraintes.sql

--- 2_contraintes.sql
+++ 2_contraintes.sql
@@ -1,3 +1,4 @@
+//caca
 ALTER TABLE Ghost_types
 ADD CONSTRAINT chk_hostility_range CHECK (hostility BETWEEN 0 AND 10);
 
@@ -13,5 +14,6 @@ ADD CONSTRAINT chk_death_date CHECK (date_of_death <= CURRENT_DATE);
 
 ALTER TABLE Ghosts
 ADD CONSTRAINT chk_ghost_names CHECK (first_name <> '' AND last_name <> '');
+
 
 
